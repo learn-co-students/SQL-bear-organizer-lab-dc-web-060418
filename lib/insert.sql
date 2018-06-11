15871145ec3c3d809a953cece121afa53e7cbfae
Essa Hijazi <github email address>
2018-06-11 10:19:21 -0400

    Created basic table and queries

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 7, "M", "Brown", "Sad", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 10, "F", "Black", "Rowdy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 4, "F", "Black", "Energetic", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Sergeant Brown", 14, "M", "Brown", "Diligent", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Melissa", 2, "F", "Brown", "Compassionate", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Grinch", 5, "M", "Black", "Agggressive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Wendy", 43, "F", "Brown", "Slow", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+(NULL, 23, "M", "Brown", "Mysterious", 1);
